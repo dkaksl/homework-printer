da0ff9e8eef64bd81a780a34093d6e33aaf3b804
Amani Kilumanga <amani.kilumanga@gmail.com>
2022-06-03 23:43:06 +0200

    implemented addition table with constructor and row data state

--- src/views/addition.js
+++ src/views/addition.js
@@ -2,11 +2,48 @@ import { Component } from "react";
 import { Printable } from "../components/printable";
 
 export class Addition extends Component {
+  constructor(props) {
+    super(props);
+    const rows = this.generateRowData(props.rowCount);
+    this.state = { rows };
+  }
+
+  generateRowData(rowCount) {
+    const rows = [];
+    for (let i = 0; i < rowCount; i++) {
+      const a = Math.floor(Math.random() * 20);
+      const b = Math.floor(Math.random() * a);
+      rows.push({ a, b });
+    }
+    return rows;
+  }
+
+  refreshRowData(rowCount) {
+    this.setState({ rows: this.generateRowData(rowCount) });
+  }
+
   render() {
     return (
-      <Printable>
-        <div>This is where the homework content should go</div>
-      </Printable>
+      <div>
+        <button onClick={() => this.refreshRowData(this.props.rowCount)}>
+          Randomize
+        </button>
+        <Printable>
+          <div>
+            <table>
+              {this.state.rows.map((row) => (
+                <tr>
+                  <th>{row.a}</th>
+                  <th>-</th>
+                  <th>{row.b}</th>
+                  <th>=</th>
+                  <th>&nbsp;</th>
+                </tr>
+              ))}
+            </table>
+          </div>
+        </Printable>
+      </div>
     );
   }
 }
