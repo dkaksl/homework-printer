68dfe6675c54af2c44d0af704e83f3e25c183308
Amani Kilumanga <amani.kilumanga@gmail.com>
2022-06-04 18:12:32 +0200

    updated styling of menu to match play.kilumanga.com; refactored app entrypoint to single component to better use state between components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import "./App.css";
 
 import { Main } from "./views/main";
-import { Menu } from "./components/menu";
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <Menu />
-      </header>
-      <Main selected="Subtraction" />
+    <div className="app">
+      <Main />
     </div>
   );
 }
