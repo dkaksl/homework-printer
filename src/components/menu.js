23ddc964dcddf8923ce12d9dd8bc6d74999fd412
Amani Kilumanga <amani.kilumanga@gmail.com>
2022-06-04 18:14:53 +0200

    refactored menu to have two items; pass selected value to parent; styling

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,7 +1,40 @@
 import { Component } from "react";
 
 export class Menu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { selected: "Subtraction" };
+  }
+
+  isActive(selected) {
+    return this.state.selected === selected ? "active" : "";
+  }
+
+  handleClick(selected) {
+    this.props.menuClickHandler(selected);
+    this.setState({ selected });
+  }
+
+  getMenuItem(name) {
+    return (
+      <a
+        className={this.isActive(name)}
+        href="#"
+        onClick={() => this.handleClick(name)}
+      >
+        {name}
+      </a>
+    );
+  }
+
   render() {
-    return <div className="menu navigation-menu">Menu goes here</div>;
+    return (
+      <div className="menu navigation-menu">
+        <ul>
+          <li>{this.getMenuItem("Subtraction")}</li>
+          <li>{this.getMenuItem("Addition")}</li>
+        </ul>
+      </div>
+    );
   }
 }
