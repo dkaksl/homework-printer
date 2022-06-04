c42e61a43c7111e3350fdbe3cd30c767202fe922
Amani Kilumanga <amani.kilumanga@gmail.com>
2022-06-04 18:14:00 +0200

    refacored main component to use state; added click handler to pass to menu

--- src/views/main.js
+++ src/views/main.js
@@ -1,20 +1,53 @@
 import { Component } from "react";
 
+import { Menu } from "../components/menu";
 import { Subtraction } from "./subtraction";
 
 export class Main extends Component {
-  render() {
-    if (this.props.selected === "Subtraction") {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: "Subtraction",
+      rowCount: 20,
+      difficulty: "medium",
+    };
+    this.menuClickHandler = this.menuClickHandler.bind(this);
+  }
+
+  menuClickHandler(value) {
+    this.setState({ selected: value });
+  }
+
+  difficultyHandler(difficulty) {
+    this.setState({ difficulty });
+  }
+
+  getHomeworkView(selected, rowCount, difficulty) {
+    if (selected === "Subtraction") {
       return (
-        <div className="content">
-          <Subtraction
-            rowCount="20"
-            difficulty={this.props.difficulty}
-          ></Subtraction>
-        </div>
+        <Subtraction rowCount={rowCount} difficulty={difficulty}></Subtraction>
       );
+    } else if (selected === "Addition") {
+      return <div>TODO</div>;
     } else {
       return <div>Select an exercise and difficulty level</div>;
     }
+  }
+
+  render() {
+    return (
+      <div>
+        <header>
+          <Menu menuClickHandler={this.menuClickHandler} />
+        </header>
+        <div className="content">
+          {this.getHomeworkView(
+            this.state.selected,
+            this.state.rowCount,
+            this.props.difficulty
+          )}
+        </div>
+      </div>
+    );
   }
 }
