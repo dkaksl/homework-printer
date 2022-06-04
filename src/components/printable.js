e9c2b22c4ae6129b9d07e320722add52b3b42fe8
Amani Kilumanga <amani.kilumanga@gmail.com>
2022-06-04 14:52:57 +0200

    wrapped printable content in react-to-print component

--- src/components/printable.js
+++ src/components/printable.js
@@ -1,12 +1,18 @@
 import { Component } from "react";
+import ReactToPrint from "react-to-print";
 
 export class Printable extends Component {
   render() {
     return (
       <div>
-        <div>{this.props.children}</div>
-        <div>
-          <button>Print This Sheet</button>
+        <ReactToPrint
+          trigger={() => {
+            return <button>Print This Sheet</button>;
+          }}
+          content={() => this.componentRef}
+        ></ReactToPrint>
+        <div className="printable" ref={(el) => (this.componentRef = el)}>
+          {this.props.children}
         </div>
       </div>
     );
