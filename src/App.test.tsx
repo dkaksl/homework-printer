613868b027a4c25e438748a9138d9ee255b48098
Amani Kilumanga <amani.kilumanga@gmail.com>
2022-06-04 23:58:45 +0200

    renamed test to something relevant

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders print button", () => {
   render(<App />);
   const printButton = screen.getByText(/Print This Sheet/i);
   expect(printButton).toBeInTheDocument();
