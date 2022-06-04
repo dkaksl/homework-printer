8f1b4e5fa1e88c82677c495bfb029888e4e68146
Amani Kilumanga <amani.kilumanga@gmail.com>
2022-06-04 18:46:15 +0200

    added handling of difficulty setting; added TODO annotation

--- src/views/subtraction.js
+++ src/views/subtraction.js
@@ -8,11 +8,29 @@ export class Subtraction extends Component {
     this.state = { rows };
   }
 
+  getMaxNumber() {
+    switch (this.props.difficulty) {
+      case "easy":
+        return 10;
+      case "medium":
+        return 15;
+      case "hard":
+        return 20;
+      case "advanced":
+        return 20;
+      default:
+        return 15;
+    }
+  }
+
   generateRowData(rowCount) {
+    const maxNumber = this.getMaxNumber();
     const rows = [];
     for (let i = 0; i < rowCount; i++) {
-      const a = Math.floor(Math.random() * 20);
-      const b = Math.floor(Math.random() * a);
+      const a = Math.floor(Math.random() * maxNumber);
+      const b = Math.floor(
+        Math.random() * (this.props.difficulty === "advanced" ? maxNumber : a)
+      );
       rows.push({ a, b });
     }
     return rows;
@@ -25,6 +43,7 @@ export class Subtraction extends Component {
   render() {
     return (
       <div>
+        {/* TODO: move randomize button to parent*/}
         <button onClick={() => this.refreshRowData(this.props.rowCount)}>
           Randomize
         </button>
