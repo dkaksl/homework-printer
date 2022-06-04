47072fea4b8bf987345e6531df6cabfce3c9f951
Amani Kilumanga <amani.kilumanga@gmail.com>
2022-06-04 22:49:33 +0200

    switched to new component and util function

--- src/views/subtraction.js
+++ src/views/subtraction.js
@@ -1,43 +1,19 @@
 import { Component } from "react";
 import { Printable } from "../components/printable";
+import { Sheet } from "../components/sheet";
+import { generateRowData } from "../util/index";
 
 export class Subtraction extends Component {
   constructor(props) {
     super(props);
-    const rows = this.generateRowData(props.rowCount);
+    const rows = generateRowData(props.rowCount, props.difficulty, "-");
     this.state = { rows };
   }
 
-  getMaxNumber() {
-    switch (this.props.difficulty) {
-      case "easy":
-        return 10;
-      case "medium":
-        return 15;
-      case "hard":
-        return 20;
-      case "advanced":
-        return 20;
-      default:
-        return 15;
-    }
-  }
-
-  generateRowData(rowCount) {
-    const maxNumber = this.getMaxNumber();
-    const rows = [];
-    for (let i = 0; i < rowCount; i++) {
-      const a = Math.floor(Math.random() * maxNumber);
-      const b = Math.floor(
-        Math.random() * (this.props.difficulty === "advanced" ? maxNumber : a)
-      );
-      rows.push({ a, b });
-    }
-    return rows;
-  }
-
   refreshRowData(rowCount) {
-    this.setState({ rows: this.generateRowData(rowCount) });
+    this.setState({
+      rows: generateRowData(rowCount, this.props.difficulty, "-"),
+    });
   }
 
   render() {
@@ -48,28 +24,7 @@ export class Subtraction extends Component {
           Randomize
         </button>
         <Printable>
-          <div>
-            <table>
-              {/* TODO: extract component */}
-              {this.state.rows.map((row) => (
-                <tr>
-                  <td>{row.a}</td>
-                  <td>-</td>
-                  <td>{row.b}</td>
-                  <td>=</td>
-                  <td>&nbsp;</td>
-                </tr>
-              ))}
-              <tr>
-                <td colSpan={2}>Name:</td>
-                <td colSpan={3}></td>
-              </tr>
-              <tr>
-                <td colSpan={2}>Date:</td>
-                <td colSpan={3}></td>
-              </tr>
-            </table>
-          </div>
+          <Sheet rows={this.state.rows}></Sheet>
         </Printable>
       </div>
     );
