1ff8b1fd49f4b83041c25d60e730cb62582a1802
Amani Kilumanga <amani.kilumanga@gmail.com>
2022-06-05 00:43:02 +0200

    created generic maths component to handle both addition and subtraction (and refresh table data at the same time difficulty is selected)

---
+++ src/views/maths.tsx
@@ -0,0 +1,77 @@
+import { Component } from "react";
+import { Printable } from "../components/printable";
+import { Sheet } from "../components/sheet";
+import {
+  generateRowData,
+  getOperatorFromSelected,
+  RowData,
+} from "../util/index";
+import { Difficulty } from "./main";
+
+interface Props {
+  selected: string;
+  rowCount: number;
+}
+
+interface State {
+  difficulty: Difficulty;
+  rows: RowData[];
+}
+
+export class Maths extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    const defaultDifficulty = "medium";
+    this.state = {
+      difficulty: defaultDifficulty,
+      rows: generateRowData(
+        this.props.rowCount,
+        defaultDifficulty,
+        getOperatorFromSelected("Subtraction")
+      ),
+    };
+    this.difficultyHandler = this.difficultyHandler.bind(this);
+  }
+
+  difficultyHandler(event: any) {
+    this.setState({ difficulty: event.target.value }, () => {
+      this.refreshRowData(this.props.rowCount);
+    });
+  }
+
+  refreshRowData(rowCount: number) {
+    this.setState({
+      rows: generateRowData(
+        rowCount,
+        this.state.difficulty,
+        getOperatorFromSelected(this.props.selected)
+      ),
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <label>
+          Difficulty:
+          <select
+            value={this.state.difficulty}
+            onChange={this.difficultyHandler}
+          >
+            {/* TODO: explain difficulties in tooltip */}
+            <option value="easy">Easy</option>
+            <option value="medium">Medium</option>
+            <option value="hard">Hard</option>
+            <option value="advanced">Advanced</option>
+          </select>
+        </label>
+        <button onClick={() => this.refreshRowData(this.props.rowCount)}>
+          Randomize
+        </button>
+        <Printable>
+          <Sheet rows={this.state.rows}></Sheet>
+        </Printable>
+      </div>
+    );
+  }
+}
