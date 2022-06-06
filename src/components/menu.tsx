ffe6fec0703babba963c88105daf8595ee59abd8
Amani Kilumanga <amani.kilumanga@gmail.com>
2022-06-06 14:43:32 +0200

    removed menu items in favor of form embedded in page

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,43 +1,10 @@
 import { Component } from "react";
 
-interface Props {
-  menuClickHandler: Function;
-}
-
-export class Menu extends Component<Props, { selected: string }> {
-  constructor(props: Props) {
-    super(props);
-    this.state = { selected: "Subtraction" };
-  }
-
-  isActive(selected: string) {
-    return this.state.selected === selected ? "active" : "";
-  }
-
-  handleClick(selected: string) {
-    this.props.menuClickHandler(selected);
-    this.setState({ selected });
-  }
-
-  getMenuItem(name: string) {
-    return (
-      <a
-        className={this.isActive(name)}
-        href="#/"
-        onClick={() => this.handleClick(name)}
-      >
-        {name}
-      </a>
-    );
-  }
-
+export class Menu extends Component {
   render() {
     return (
-      <div className="menu navigation-menu">
-        <ul>
-          <li>{this.getMenuItem("Subtraction")}</li>
-          <li>{this.getMenuItem("Addition")}</li>
-        </ul>
+      <div className="menu">
+        <h1>Homework Printer</h1>
       </div>
     );
   }
