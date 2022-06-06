480e78443dd4d80316d7f13858d9c9b17d751c31
Amani Kilumanga <amani.kilumanga@gmail.com>
2022-06-06 14:44:30 +0200

    removed handling of menu items; added form with checkboxes for included operators; changed to generate forms based on selected checkboxes

--- src/components/maths.tsx
+++ src/components/maths.tsx
@@ -1,21 +1,19 @@
 import { Component } from "react";
 import { Printable } from "./printable";
 import { Sheet } from "./sheet";
-import {
-  generateRowData,
-  getOperatorFromSelected,
-  RowData,
-} from "../util/index";
-import { Difficulty } from "../views/main";
+import { generateRowData, RowData } from "../util/index";
+import { Difficulty, Operator } from "../views/main";
+import { Checkbox } from "./checkbox";
 
 interface Props {
-  selected: string;
   rowCount: number;
 }
 
 interface State {
   difficulty: Difficulty;
   rows: RowData[];
+  plus: boolean;
+  minus: boolean;
 }
 
 export class Maths extends Component<Props, State> {
@@ -24,11 +22,25 @@ export class Maths extends Component<Props, State> {
     const defaultDifficulty = "medium";
     this.state = {
       difficulty: defaultDifficulty,
-      rows: generateRowData(this.props.rowCount, defaultDifficulty, [
-        getOperatorFromSelected("Subtraction"),
-      ]),
+      plus: true,
+      minus: false,
+      rows: generateRowData(this.props.rowCount, defaultDifficulty, ["+"]),
     };
     this.difficultyHandler = this.difficultyHandler.bind(this);
+    this.togglePlus = this.togglePlus.bind(this);
+    this.toggleMinus = this.toggleMinus.bind(this);
+  }
+
+  togglePlus() {
+    this.setState({
+      plus: !this.state.plus,
+    });
+  }
+
+  toggleMinus() {
+    this.setState({
+      minus: !this.state.minus,
+    });
   }
 
   difficultyHandler(event: any) {
@@ -37,14 +49,26 @@ export class Maths extends Component<Props, State> {
     });
   }
 
+  getSelectedOperators() {
+    const operators: Operator[] = [];
+    if (this.state.plus) {
+      operators.push("+");
+    }
+    if (this.state.minus) {
+      operators.push("-");
+    }
+    if (operators.length < 1) {
+      operators.push("+");
+    }
+    return operators;
+  }
+
   refreshRowData(rowCount: number) {
     this.setState({
       rows: generateRowData(
         rowCount,
         this.state.difficulty,
-        this.props.selected === "Mix"
-          ? ["-", "+"] // TODO: allow user to select operators
-          : [getOperatorFromSelected(this.props.selected)]
+        this.getSelectedOperators()
       ),
     });
   }
@@ -52,22 +76,47 @@ export class Maths extends Component<Props, State> {
   render() {
     return (
       <div>
-        <label>
-          Difficulty:
-          <select
-            value={this.state.difficulty}
-            onChange={this.difficultyHandler}
-          >
-            {/* TODO: explain difficulties in tooltip */}
-            <option value="easy">Easy</option>
-            <option value="medium">Medium</option>
-            <option value="hard">Hard</option>
-            <option value="advanced">Advanced</option>
-          </select>
-        </label>
-        <button onClick={() => this.refreshRowData(this.props.rowCount)}>
-          Randomize
-        </button>
+        <section className="form-parent">
+          <form className="form">
+            <label htmlFor="difficulty-selector">Difficulty</label>
+            <select
+              id="difficulty-selector"
+              value={this.state.difficulty}
+              onChange={this.difficultyHandler}
+            >
+              {/* TODO: explain difficulties in tooltip */}
+              <option value="easy">Easy</option>
+              <option value="medium">Medium</option>
+              <option value="hard">Hard</option>
+              <option value="advanced">Advanced</option>
+            </select>
+
+            {/* <input id="number" type="number" value="1"></input> */}
+            <label htmlFor="checkbox-group">Included Operators</label>
+            <div id="checkbox-group">
+              <Checkbox
+                defaultChecked={true}
+                name="plus"
+                operator="+"
+                toggle={this.togglePlus}
+              ></Checkbox>
+              <Checkbox
+                defaultChecked={false}
+                name="minus"
+                operator="-"
+                toggle={this.toggleMinus}
+              ></Checkbox>
+            </div>
+
+            <button
+              type="button"
+              onClick={() => this.refreshRowData(this.props.rowCount)}
+            >
+              Randomize
+            </button>
+          </form>
+        </section>
+
         <Printable>
           <Sheet rows={this.state.rows}></Sheet>
         </Printable>
