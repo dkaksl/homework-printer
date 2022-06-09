9c73beda5377f94c57c73c48a03ad5d12378c28d
Amani Kilumanga <amani.kilumanga@gmail.com>
2022-06-09 19:39:46 +0200

    reimplemented menu with navigation

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,10 +1,49 @@
 import { Component } from "react";
 
-export class Menu extends Component {
+interface Props {
+  selected: string;
+  navigationHandler: Function;
+}
+
+interface State {
+  selected: string;
+}
+export class Menu extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { selected: this.props.selected };
+    this.menuClickHandler = this.menuClickHandler.bind(this);
+  }
+
+  menuClickHandler(selected: string) {
+    this.props.navigationHandler(selected);
+    this.setState({ selected });
+    return undefined;
+  }
+
   render() {
     return (
       <div className="menu">
-        <h1>Homework Printer</h1>
+        <ul>
+          <li>
+            <a
+              href="#/"
+              className={this.state.selected === "maths" ? "active" : ""}
+              onClick={() => this.menuClickHandler("maths")}
+            >
+              Maths
+            </a>
+          </li>
+          <li>
+            <a
+              href="#/"
+              className={this.state.selected === "abcs" ? "active" : ""}
+              onClick={() => this.menuClickHandler("abcs")}
+            >
+              ABCs
+            </a>
+          </li>
+        </ul>
       </div>
     );
   }
