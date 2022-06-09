1a3179c41af5805b5c9f10c89e9d0efbcbb5d679
Amani Kilumanga <amani.kilumanga@gmail.com>
2022-06-09 22:35:17 +0200

    added i18n config & translations

---
+++ src/i18n/config.ts
@@ -0,0 +1,11 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import resources from "./translation.json";
+
+i18n.use(initReactI18next).init({
+  fallbackLng: "en",
+  interpolation: {
+    escapeValue: false, // not needed for react as it escapes by default
+  },
+  resources,
+});
