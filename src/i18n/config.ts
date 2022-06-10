27860265a157b488633ce9b28f7908165fde08fc
Amani Kilumanga <amani.kilumanga@gmail.com>
2022-06-10 20:43:28 +0200

    updated translations import

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,11 +1,11 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import resources from "./translation.json";
+import { resources } from "./translation";
 
 i18n.use(initReactI18next).init({
   fallbackLng: "en",
   interpolation: {
     escapeValue: false, // not needed for react as it escapes by default
   },
-  resources,
+  resources: resources as any,
 });
