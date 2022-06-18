bcf8e101899e2b359cd40f28f70f9b23a184b334
Amani Kilumanga <amani.kilumanga@gmail.com>
2022-06-18 14:53:49 +0200

    extracted some functions in abcs sheet; added uppercase and lowercase toggle

--- src/components/abcs.tsx
+++ src/components/abcs.tsx
@@ -8,7 +8,9 @@ import { withTranslation, WithTranslation } from "react-i18next";
 interface Props extends WithTranslation {}
 
 interface State {
+  rows: { leftColumn: string; rightColumn: string }[];
   difficulty: Difficulty;
+  case: "upper" | "lower";
 }
 
 const capitalABCColumns = {
@@ -16,9 +18,9 @@ const capitalABCColumns = {
   rightColumn: "UVWXYZÅÄÖ0123456789?",
 };
 
-const getCapitalABCRowString = () => {
-  const leftColumnChars = capitalABCColumns.leftColumn.split("");
-  const rightColumnChars = capitalABCColumns.rightColumn.split("");
+const getRowString = (columns: { leftColumn: string; rightColumn: string }) => {
+  const leftColumnChars = columns.leftColumn.split("");
+  const rightColumnChars = columns.rightColumn.split("");
   const newStringArray = [];
   for (let i = 0; i < leftColumnChars.length; i++) {
     newStringArray.push(leftColumnChars[i]);
@@ -27,18 +29,63 @@ const getCapitalABCRowString = () => {
   return newStringArray.join("");
 };
 
+const getCapitalABCRowString = () => {
+  return getRowString(capitalABCColumns);
+};
+
+const getLowercaseABCRowString = () => {
+  return getRowString({
+    leftColumn: capitalABCColumns.leftColumn.toLocaleLowerCase(),
+    rightColumn: capitalABCColumns.rightColumn.toLocaleLowerCase(),
+  });
+};
+
 class ABCs extends Component<Props, State> {
   constructor(props: Props) {
     super(props);
     const defaultDifficulty = "medium";
+    const defaultCase = "upper";
+    const defaultRows =
+      getCapitalABCRowString()
+        .match(/.{2}/g)
+        ?.map((row) => ({
+          leftColumn: row[0],
+          rightColumn: row[1],
+        })) || [];
     this.state = {
+      rows: defaultRows,
       difficulty: defaultDifficulty,
+      case: defaultCase,
     };
     this.difficultyHandler = this.difficultyHandler.bind(this);
+    this.caseHandler = this.caseHandler.bind(this);
+  }
+
+  generateRows() {
+    const rowString =
+      this.state.case === "upper"
+        ? getCapitalABCRowString()
+        : getLowercaseABCRowString();
+    return (
+      rowString.match(/.{2}/g)?.map((row) => ({
+        leftColumn: row[0],
+        rightColumn: row[1],
+      })) || []
+    );
+  }
+
+  refreshRows() {
+    this.setState({ rows: this.generateRows() });
   }
 
   difficultyHandler(event: any) {
     this.setState({ difficulty: event.target.value });
+  }
+
+  caseHandler(event: any) {
+    this.setState({ case: event.target.value }, () => {
+      this.refreshRows();
+    });
   }
 
   render() {
@@ -61,6 +108,22 @@ class ABCs extends Component<Props, State> {
                 {this.props.t<string>("Advanced")}
               </option>
             </select>
+            <label htmlFor="upper">{this.props.t<string>("uppercase")}</label>
+            <input
+              type="radio"
+              id="upper"
+              name="case"
+              value="upper"
+              onChange={this.caseHandler}
+            />
+            <label htmlFor="upper">{this.props.t<string>("lowercase")}</label>
+            <input
+              type="radio"
+              id="lower"
+              name="case"
+              value="lower"
+              onChange={this.caseHandler}
+            />
           </form>
         </section>
 
@@ -69,14 +132,7 @@ class ABCs extends Component<Props, State> {
             <div className="page-break" />
             <ABCSheet
               difficulty={this.state.difficulty}
-              rows={
-                getCapitalABCRowString()
-                  .match(/.{2}/g)
-                  ?.map((row) => ({
-                    leftColumn: row[0],
-                    rightColumn: row[1],
-                  })) || []
-              }
+              rows={this.state.rows}
             ></ABCSheet>
           </div>
         </Printable>
