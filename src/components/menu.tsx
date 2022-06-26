4ba6ecf73f93d2627d046d2e3cf15a9385f88a9c
Amani Kilumanga <amani.kilumanga@gmail.com>
2022-06-26 19:21:30 +0200

    formatting

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,26 +1,26 @@
-import { Component } from "react";
-import { withTranslation, WithTranslation } from "react-i18next";
+import { Component } from 'react'
+import { withTranslation, WithTranslation } from 'react-i18next'
 
 interface Props extends WithTranslation {
-  selected: string;
-  navigationHandler: Function;
+  selected: string
+  navigationHandler: Function
 }
 
 interface State {
-  selected: string;
+  selected: string
 }
 
 class Menu extends Component<Props, State> {
   constructor(props: Props) {
-    super(props);
-    this.state = { selected: this.props.selected };
-    this.menuClickHandler = this.menuClickHandler.bind(this);
+    super(props)
+    this.state = { selected: this.props.selected }
+    this.menuClickHandler = this.menuClickHandler.bind(this)
   }
 
   menuClickHandler(selected: string) {
-    this.props.navigationHandler(selected);
-    this.setState({ selected });
-    return undefined;
+    this.props.navigationHandler(selected)
+    this.setState({ selected })
+    return undefined
   }
 
   render() {
@@ -31,22 +31,22 @@ class Menu extends Component<Props, State> {
             <button
               className="fib fi-gb"
               onClick={() => {
-                this.props.i18n.changeLanguage("en");
-                localStorage.setItem("i18nextLng", "en");
+                this.props.i18n.changeLanguage('en')
+                localStorage.setItem('i18nextLng', 'en')
               }}
             ></button>
             <button
               className="fib fi-se"
               onClick={() => {
-                this.props.i18n.changeLanguage("se");
-                localStorage.setItem("i18nextLng", "se");
+                this.props.i18n.changeLanguage('se')
+                localStorage.setItem('i18nextLng', 'se')
               }}
             ></button>
             <button
               className="fib fi-tw"
               onClick={() => {
-                this.props.i18n.changeLanguage("cn");
-                localStorage.setItem("i18nextLng", "cn");
+                this.props.i18n.changeLanguage('cn')
+                localStorage.setItem('i18nextLng', 'cn')
               }}
             ></button>
           </div>
@@ -55,25 +55,25 @@ class Menu extends Component<Props, State> {
           <li>
             <a
               href="#/"
-              className={this.state.selected === "maths" ? "active" : ""}
-              onClick={() => this.menuClickHandler("maths")}
+              className={this.state.selected === 'maths' ? 'active' : ''}
+              onClick={() => this.menuClickHandler('maths')}
             >
-              {this.props.t<string>("Maths")}
+              {this.props.t<string>('Maths')}
             </a>
           </li>
           <li>
             <a
               href="#/"
-              className={this.state.selected === "abcs" ? "active" : ""}
-              onClick={() => this.menuClickHandler("abcs")}
+              className={this.state.selected === 'abcs' ? 'active' : ''}
+              onClick={() => this.menuClickHandler('abcs')}
             >
-              {this.props.t<string>("ABCs")}
+              {this.props.t<string>('ABCs')}
             </a>
           </li>
         </ul>
       </div>
-    );
+    )
   }
 }
 
-export default withTranslation()(Menu);
+export default withTranslation()(Menu)
