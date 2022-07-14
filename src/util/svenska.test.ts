db0f50c64ffe4e41aa6345234816c91ef26985ac
Amani Kilumanga <amani.kilumanga@gmail.com>
2022-07-14 17:50:19 +0200

    added swedish dictionary

---
+++ src/util/svenska.test.ts
@@ -0,0 +1,12 @@
+import { ordbok } from './svenska'
+
+describe('ordbok tests', () => {
+  test('that there are no duplicate nouns', () => {
+    const substantiv = ordbok.substantiv
+    const substantivSet = new Set()
+    for (const s of substantiv) {
+      substantivSet.add(s.ord)
+    }
+    expect(substantiv.length).toEqual(substantivSet.size)
+  })
+})
