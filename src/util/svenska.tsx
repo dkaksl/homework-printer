2b0591f76a154b522ee808a6eff3ddb7484ef738
Amani Kilumanga <amani.kilumanga@gmail.com>
2022-07-15 18:48:20 +0200

    added random programmatic numerator in plural nouns

--- src/util/svenska.tsx
+++ src/util/svenska.tsx
@@ -11,6 +11,8 @@ export enum SubstantivAlternativ {
   PluralBestämd = 'plural-bestämd'
 }
 
+const pluralNummer = ['två', 'tre', 'fyra', 'fem', 'sex', 'sju', 'åtta', 'nio']
+
 interface Substantiv extends Ord {
   singular: { obestämd: string; bestämd: string }
   plural: { obestämd: string; bestämd: string }
@@ -29,17 +31,17 @@ const möbler: Substantiv[] = [
   {
     ord: 'bord',
     singular: { obestämd: 'ett bord', bestämd: 'det bordet' },
-    plural: { obestämd: 'bord', bestämd: 'de borden' }
+    plural: { obestämd: 'bord', bestämd: 'borden' }
   },
   {
     ord: 'bokhylla',
     singular: { obestämd: 'en bokhylla', bestämd: 'den bokhyllan' },
-    plural: { obestämd: 'bokhyllor', bestämd: 'de bokhyllorna' }
+    plural: { obestämd: 'bokhyllor', bestämd: 'bokhyllorna' }
   },
   {
     ord: 'soffa',
     singular: { obestämd: 'en soffa', bestämd: 'den soffan' },
-    plural: { obestämd: 'soffor', bestämd: 'de sofforna' }
+    plural: { obestämd: 'soffor', bestämd: 'sofforna' }
   }
 ]
 
@@ -47,12 +49,12 @@ const redskap: Substantiv[] = [
   {
     ord: 'penna',
     singular: { obestämd: 'en penna', bestämd: 'den pennan' },
-    plural: { obestämd: 'pennor', bestämd: 'de pennorna' }
+    plural: { obestämd: 'pennor', bestämd: 'pennorna' }
   },
   {
     ord: 'papper',
     singular: { obestämd: 'ett papper', bestämd: 'det pappret' },
-    plural: { obestämd: 'papper', bestämd: 'de pappren' }
+    plural: { obestämd: 'papper', bestämd: 'pappren' }
   }
 ]
 
@@ -60,27 +62,27 @@ const kläder: Substantiv[] = [
   {
     ord: 'sko',
     singular: { obestämd: 'en sko', bestämd: 'den skon' },
-    plural: { obestämd: 'skor', bestämd: 'de skorna' }
+    plural: { obestämd: 'skor', bestämd: 'skorna' }
   },
   {
     ord: 'strumpa',
     singular: { obestämd: 'en strumpa', bestämd: 'den strumpan' },
-    plural: { obestämd: 'strumpor', bestämd: 'de strumporna' }
+    plural: { obestämd: 'strumpor', bestämd: 'strumporna' }
   },
   {
     ord: 'byxa',
     singular: { obestämd: 'en byxa', bestämd: 'den byxan' },
-    plural: { obestämd: 'byxor', bestämd: 'de byxorna' }
+    plural: { obestämd: 'byxor', bestämd: 'byxorna' }
   },
   {
     ord: 'tröja',
     singular: { obestämd: 'en tröja', bestämd: 'den tröjan' },
-    plural: { obestämd: 'tröjor', bestämd: 'de tröjorna' }
+    plural: { obestämd: 'tröjor', bestämd: 'tröjorna' }
   },
   {
     ord: 'jacka',
     singular: { obestämd: 'en jacka', bestämd: 'den jackan' },
-    plural: { obestämd: 'jackor', bestämd: 'de jackorna' }
+    plural: { obestämd: 'jackor', bestämd: 'jackorna' }
   }
 ]
 
@@ -88,17 +90,17 @@ const mat: Substantiv[] = [
   {
     ord: 'äpple',
     singular: { obestämd: 'ett äpple', bestämd: 'det äpplet' },
-    plural: { obestämd: 'äpplen', bestämd: 'de äpplena' }
+    plural: { obestämd: 'äpplen', bestämd: 'äpplena' }
   },
   {
     ord: 'päron',
     singular: { obestämd: 'ett päron', bestämd: 'det päronet' },
-    plural: { obestämd: 'päron', bestämd: 'de päronen' }
+    plural: { obestämd: 'päron', bestämd: 'päronen' }
   },
   {
     ord: 'banan',
     singular: { obestämd: 'en banan', bestämd: 'den bananen' },
-    plural: { obestämd: 'bananer', bestämd: 'de bananerna' }
+    plural: { obestämd: 'bananer', bestämd: 'bananerna' }
   }
 ]
 
@@ -120,7 +122,16 @@ export const ordbok: {
 
 export const getRandomNoun = () => {
   const substantiv = ordbok.substantiv
-  return substantiv[Math.floor(Math.random() * substantiv.length)]
+  const randomSubstantiv =
+    substantiv[Math.floor(Math.random() * substantiv.length)]
+  const randomSubstantivCopy = JSON.parse(JSON.stringify(randomSubstantiv))
+  const randomPluralNummer =
+    pluralNummer[Math.floor(Math.random() * pluralNummer.length)]
+  randomSubstantivCopy.plural.obestämd =
+    randomPluralNummer + ' ' + randomSubstantiv.plural.obestämd
+  randomSubstantivCopy.plural.bestämd =
+    'de ' + randomPluralNummer + ' ' + randomSubstantiv.plural.bestämd
+  return randomSubstantivCopy
 }
 
 export const getRandomVerb = () => {
