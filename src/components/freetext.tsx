10f2c410ce4dc89b3c159d0596d3112d385340f0
Amani Kilumanga <amani.kilumanga@gmail.com>
2023-02-17 21:53:35 +0100

    feat: added textarea component for freetext

---
+++ src/components/freetext.tsx
@@ -0,0 +1,29 @@
+import { Component } from 'react'
+
+interface Props {
+  label: string
+  name?: string
+  text?: string
+}
+
+export class Freetext extends Component<Props> {
+  constructor(props: Props) {
+    super(props)
+  }
+
+  render() {
+    const id = `textarea-${this.props.name}`
+    return (
+      <div className='freetext-parent'>
+        <label className='freetext-child' htmlFor={id}>{this.props.label}:</label>
+        <textarea
+          className='freetext-child'
+          id={id}
+          name={id}
+          rows={4}
+          cols={50}
+        >{this.props.text}</textarea>
+      </div >
+    )
+  }
+}
